16c7c80a90b677d01dc6aca11fe15d7f14e832f4
Dobinoo <dobinoo5@gmail.com>
2018-07-25 22:25:44 +0100

    This feature not working right now

--- subprocess.py
+++ subprocess.py
@@ -1,4 +1,6 @@
-from subprocess import call
-call(["ls", "-l"])
+#from subprocess import call
+#call(["ls", "-l"])
 #url = "https://www.brainyquote.com/topics/daily"
 #call(["wget",url])
+
+print("Iam Not Working Right NOW")
